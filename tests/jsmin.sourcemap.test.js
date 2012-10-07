eb2c50e3dfccf9672968dae6abe4f1aa66a80b05
Todd Wolfson <todd@twolfson.com>
2012-10-07 03:15:08 -0700

    Compiling multiple files together

--- tests/jsmin.sourcemap.test.js
+++ tests/jsmin.sourcemap.test.js
@@ -11,5 +11,31 @@ var fs = require('fs'),
 // Assert that the minified jQuery matches the expected version
 assert.strictEqual(expectedJQuery, actualJQueryCode, 'Minified jQuery does not match as expected.');
 
+// TODO: Add reversal test for sourcemap -- do all the characters line up?
+
+// Grab underscore
+var _Src = fs.readFileSync(testFilesDir + '/underscore.js', 'utf8'),
+    minParams = {
+      'input': [{
+        'code': jQuerySrc,
+        'src': 'jquery.js'
+      }, {
+        'code': _Src,
+        'src': 'underscore.js'
+      }],
+      'dest': 'jqueryAndUnderscore.min.js'
+    },
+    expectedJQueryAnd_Code = fs.readFileSync(expectedDir + '/jqueryAndUnderscore.min.js', 'utf8'),
+    actualJQueryAnd_ = jsmin(minParams),
+    actualJQueryAnd_Code = actualJQueryAnd_.code;
+
+// Output to combination file (debug only)
+// fs.writeFileSync('debug.min.js', actualJQueryAnd_Code, 'utf8');
+
+// Assert that the minified jQuery and underscore matches the expected version
+assert.strictEqual(expectedJQueryAnd_Code, actualJQueryAnd_Code, 'Minified jQuery and underscore do not match as expected.');
+
+// TODO: Add reversal test for sourcemap -- do all the characters line up?
+
 // Log success when done
 console.log('Success!');
