2bfaab7387fe7c7553d2e9ad3e703c0ab69b5149
Todd Wolfson <todd@twolfson.com>
2012-11-03 18:37:48 -0700

    Testing on vows

--- tests/jsmin.sourcemap.test.js
+++ tests/jsmin.sourcemap.test.js
@@ -1,32 +1,48 @@
-module.exports = [{
-  "jQuery": {
-    "minified and sourcemapped": {
-      "matches its C-minified counterpart": true,
-      // "is debuggable": true,
-      "mapped against its source": {
-        "matches at all positions": true
-      }
+var vows = require('vows'),
+    commands = require('./jsmin.sourcemap.commands.js');
+
+var suite = vows.describe('jsmin-sourcemap');
+
+suite.addBatch({
+  'test': {
+    'test2': function () {
+      return 2;
     }
   }
-}, {
-  "jQuery and Underscore": {
-    "minified and sourcemapped": {
-      "matches its C-minified counterpart": true,
-      // "is debuggable": true,
-      "mapped against its source": {
-        "matches at all positions": true
-      }
-    }
-  }
-}, {
-  "Multiple files": {
-    "minified and sourcemapped": {
-      "matches its C-minified counterpart": true,
-      // "is debuggable": true,
-      "mapped against its source": {
-        "matches at all positions": true
-      }
-    }
-  }
-}];
-// TODO: Multi-nested
+});
+
+// Export the batch as a suite
+suite['export'](module);
+
+// module.exports = [{
+//   "jQuery": {
+//     "minified and sourcemapped": {
+//       "matches its C-minified counterpart": true
+//       // // "is debuggable": true,
+//       // "mapped against its source": {
+//       //   "matches at all positions": true
+//       // }
+//     }
+//   }
+// // }, {
+// //   "jQuery and Underscore": {
+// //     "minified and sourcemapped": {
+// //       "matches its C-minified counterpart": true,
+// //       // "is debuggable": true,
+// //       "mapped against its source": {
+// //         "matches at all positions": true
+// //       }
+// //     }
+// //   }
+// // }, {
+// //   "Multiple files": {
+// //     "minified and sourcemapped": {
+// //       "matches its C-minified counterpart": true,
+// //       // "is debuggable": true,
+// //       "mapped against its source": {
+// //         "matches at all positions": true
+// //       }
+// //     }
+// //   }
+// }];
+// // TODO: Multi-nested
